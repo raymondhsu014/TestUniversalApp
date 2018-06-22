987b19808f20a6992c3ede97b69ccfdb0fcd94fa
Raymond Hsu <raymond.hsu@kdi.kongsberg.com>
2018-06-22 18:37:16 +0530

    Changes to add rectangles dynamically

--- TestUniversalApp/MainPage.xaml.cs
+++ TestUniversalApp/MainPage.xaml.cs
@@ -26,52 +26,85 @@ namespace TestUniversalApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int _updateUIwaitInMs = 50;
         Random _rand = new Random(DateTime.Now.Millisecond);
+        int _numberOfRectangleInRow = 100;
+        int _numberOfRectangleInCol = 140;
 
         public MainPage()
         {
             try
             {
                 this.InitializeComponent();
-                // Task
-                // Better: Create and start the task in one operation. 
+                FillGrid();
+                // Task Start a background task
                 Task taskA = Task.Factory.StartNew(() =>
                                                         RandomDisplayAsync()
                                                         );
 
-
-                //taskA.Start();//.Wait();
             }
             catch (Exception ex)
             {
                 //TODO
             }
-          
+        }
+
+        private void FillGrid()
+        {
+            //Adding rectangle in grid
+            for (int index = 0; index < _numberOfRectangleInRow; index++)
+            {
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(1, GridUnitType.Star);
+                gMain.RowDefinitions.Add(row);
+            }
+
+            for (int index = 0; index < _numberOfRectangleInCol; index++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                col.Width = new GridLength(1, GridUnitType.Star);
+                gMain.ColumnDefinitions.Add(col);
+            }
+
+            for (int r = 0; r < _numberOfRectangleInRow; r++)
+            {
+                for (int c = 0; c < _numberOfRectangleInCol; c++)
+                {
+                    Rectangle rect = new Rectangle();
+                    Grid.SetColumn(rect, c);
+                    Grid.SetRow(rect, r);
+                    ChangeColour(rect, true);
+                    gMain.Children.Add(rect);
+                }
+            }
         }
 
         private async Task RandomDisplayAsync()
         {
             try
-            {
+            {   //TODO
                 while (true)
                 {
+                    //Update UI in UI thread
                     await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync
                         (CoreDispatcherPriority.Normal,
                         () =>
                                 {
                                     // Your UI update code goes here!
-                                    ChangeColour(r1);
-                                    ChangeColour(r2);
-                                    ChangeColour(r3);
-                                    ChangeColour(r4);
-                                    ChangeColour(r5);
-                                    ChangeColour(r6);
-                                    ChangeColour(r7);
-                                    ChangeColour(r8);
-                                    ChangeColour(r9);
+                                    //ChangeColour(r1);
+                                    //ChangeColour(r2);
+                                    //ChangeColour(r3);
+                                    //ChangeColour(r4);
+                                    //ChangeColour(r5);
+                                    //ChangeColour(r6);
+                                    //ChangeColour(r7);
+                                    //ChangeColour(r8);
+                                    //ChangeColour(r9);
+
+                                    RandomDisplay();
                                 }
                         );
-                    System.Threading.Tasks.Task.Delay(100).Wait();
+                    System.Threading.Tasks.Task.Delay(_updateUIwaitInMs).Wait();
                 };
             }
             catch (Exception ex)
@@ -80,23 +113,54 @@ namespace TestUniversalApp
             }
         }
 
-        private void ChangeColour(Rectangle rectControl)
+        private void RandomDisplay()
+        {
+            try
+            {
+                int index = _rand.Next(0, _numberOfRectangleInRow * _numberOfRectangleInCol - 1);
+
+                if (index < gMain.Children.Count)
+                {
+                    Rectangle rect = gMain.Children[index] as Rectangle;
+                    if (rect != null)
+                    {
+                        ChangeColour(rect,false);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO
+            }
+        }
+
+        private void ChangeColour(Rectangle rectControl, bool changeOnlyTransparency)
         {
             int n1 = _rand.Next(0, 255);
-            int n2= _rand.Next(0, 255);
-            int n3 = _rand.Next(0, 255);
-            int n4 = _rand.Next(0, 255);
+           
 
             byte a = byte.Parse(n1.ToString());
-            byte r = byte.Parse(n2.ToString());
-            byte g = byte.Parse(n3.ToString());
-            byte b = byte.Parse(n4.ToString());
-
+         
             SolidColorBrush mySolidColorBrush = new SolidColorBrush();
             // Describes the brush's color using RGB values. 
             // Each value has a range of 0-255.
-            mySolidColorBrush.Color = Color.FromArgb(a, r, g, b);
+            if (changeOnlyTransparency)
+            {
+                mySolidColorBrush.Color = Color.FromArgb(a, 100, 100, 100);
+            }
+            else
+            {
+                int n2 = _rand.Next(0, 255);
+                int n3 = _rand.Next(0, 255);
+                int n4 = _rand.Next(0, 255);
+                byte r = byte.Parse(n2.ToString());
+                byte g = byte.Parse(n3.ToString());
+                byte b = byte.Parse(n4.ToString());
 
+                mySolidColorBrush.Color = Color.FromArgb(a, r, g, b);
+            }
+            
+            //mySolidColorBrush.Color = Color.FromArgb(a, 100, 100, 100);
 
             rectControl.Fill = mySolidColorBrush;
         }
